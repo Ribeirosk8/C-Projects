b67f290d46324640ae5d4a9b8842f1e388e82220
Amauri Ribeiro <115406724+Ribeirosk8@users.noreply.github.com>
2024-10-13 05:54:52 -0300

    Update mario.c

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,30 +1,24 @@
 #include <stdio.h>
 
-int main(void)
-{
-    // Prompt the user for the pyramid's height
-    int height, row, column, space;
-    do
-    {
-        height = get_int("Height: ");
-    }
-    while (height < 1 || height > 8);
+int main() {
+    int n;
 
-    for (row = 0; row < height; row++)
-    {
-        for (space = 0; space < height - row - 1; space++)
-        {
+    printf("Enter the height of the pyramid: ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) {
+        // Calculate the number of spaces needed for alignment
+        int spaces = n - i;
+
+        // Print spaces followed by hashes
+        for (int j = 0; j < spaces; j++) {
             printf(" ");
         }
-        for (column = 0; column <= row; column++)
-        {
-            printf("#");
-        }
-        printf("  ");
-        for (column = 0; column <= row; column++)
-        {
+        for (int j = 0; j < 2 * i - 1; j++) {
             printf("#");
         }
         printf("\n");
     }
+
+    return 0;
 }
